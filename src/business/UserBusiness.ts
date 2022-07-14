4d300e9af356a505acdfcfba9adc1f056c3892fd
Laura Amancio <lauran.amancio@yahoo.com.br>
2022-07-14 10:32:00 -0300

    esqueleto

---
+++ src/business/UserBusiness.ts
@@ -0,0 +1,13 @@
+import { UserInputDTO, LoginInputDTO } from "../model/User";
+
+export class UserBusiness {
+
+    async signUp(user: UserInputDTO) {
+
+    }
+
+    async login(user: LoginInputDTO) {
+
+    
+    }
+}
