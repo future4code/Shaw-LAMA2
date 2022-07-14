5f476cc91ed2918c72472f3cb1e78e3d50010e84
Sérgio Pereira Dias Júnior <99135090+Sergiopdias@users.noreply.github.com>
2022-07-14 13:12:48 -0300

    login user

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -1,13 +1,50 @@
-import { UserInputDTO, LoginInputDTO } from "../model/User";
+import { UserDatabase } from "../data/UserDatabase";
+import { BaseError } from "../error/BaseError";
+import { UserInputDTO, LoginInputDTO, User, GetUserByEmail } from "../model/User";
+import { Authenticator } from "../services/Authenticator";
+import { HashManager } from "../services/HashManager";
 
 export class UserBusiness {
+
+    constructor(
+        private userDataBase = new UserDatabase,
+        private hashManager = new HashManager,
+        private authenticator = new Authenticator
+    ) { }
 
     async signUp(user: UserInputDTO) {
 
     }
 
-    async login(user: LoginInputDTO) {
+    async login(userLogin: LoginInputDTO) {
+        const { email, password } = userLogin
+        try {
+            // testar parametros recebidos 
+            if (!email || !password) {
+                throw new BaseError(422, "Missing input");
+            }
 
-    
+            // verificar se existe user no bd 
+            const user: GetUserByEmail | undefined = await this.userDataBase.getUserByEmail(email)
+            console.log(user)
+            if (!user) {
+                throw new BaseError(404, "Invalid email.");
+            }
+
+            // verificar password 
+            const verifyPassword: boolean = await this.hashManager.compare(password, user.password)
+            // if (!verifyPassword) {
+            //     throw new BaseError(404, "Invalid password.");
+            // }
+
+            // gerar token 
+            const token: string = this.authenticator.generateToken({ id: user.id, role: user.role })
+
+            // retorno da função
+            return { token: token }
+
+        } catch (error: any) {
+            throw new BaseError(error.statusCode || 400, error.message);
+        }
     }
 }
--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,28 +1,35 @@
 import { Request, Response } from "express";
-import { BaseDatabase } from "../data/BaseDatabase";
+import { UserBusiness } from "../business/UserBusiness";
+import { LoginInputDTO } from "../model/User";
 
 export class UserController {
+
+    constructor(
+        private userBusiness = new UserBusiness
+    ) { }
+
     async signup(req: Request, res: Response) {
         try {
 
 
         } catch (error) {
-            
-        }
 
-        await BaseDatabase.destroyConnection()
+        }
     }
 
-    async login(req: Request, res: Response) {
+    login = async (req: Request, res: Response) => {
+        // receber os dados do body 
+        const { email, password } = req.body
+        const userLogin: LoginInputDTO = { email, password }
 
         try {
+            const result = await this.userBusiness.login(userLogin)
+            res.status(200).send(result)
+        } catch (error: any) {
 
-
-        } catch (error) {
+            res.status(error.statusCode || 400).send({ message: error.message });
 
         }
-
-        await BaseDatabase.destroyConnection()
     }
 
 }
